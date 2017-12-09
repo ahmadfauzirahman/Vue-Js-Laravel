47ecc3a7f2f0c6d4bf5ed795f9be73474962b56b
AHMAD FAUZI RAHMAN <ahmadfauzirahman99@gmail.com>
2017-12-09 21:28:44 +0700

    Initial commit

---
+++ resources/assets/js/components/Addpost.vue
@@ -0,0 +1,32 @@
+<template id="add-post">
+    <div>
+        <h3>Add New Post</h3>
+        <form v-on:submit.prevent="createPost">
+            <div class="form-group">
+                <label for="add-title">Title</label>
+                <input id="add-title" v-model="post.title" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="add-body">Body</label>
+                <textarea class="form-control" id="add-body" v-model="post.body" rows="10"></textarea>
+            </div>
+            <button type="submit" class="btn btn-xs btn-primary">Create Post</button>
+            <router-link class="btn btn-xs btn-warning" v-bind:to="'/'">Cancel</router-link>
+        </form>
+    </div>
+</template>
+<script>
+    export default{
+        data:function () {
+            return {post:{title:'',body:''}}
+        },
+        methods : {
+            createPost : function () {
+                let uri = 'http://localhost:8000/posts/';
+                Axios.post(uri,this.post).then((response)=>{
+                    this.$router.push({name:'Listposts'})
+                });
+            }
+        }
+    }
+</script>
